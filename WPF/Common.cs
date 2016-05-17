b4757989527ecb6f241ba1ea06b9c8d0ba267370
jemkot <pavel.klymovych@gmail.com>
2016-05-17 13:16:13 +0300

    common UI helpers

---
+++ WPF/Common.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using PQS.Data.Configurations;
+using PQS.Translator;
+
+namespace PQS.CommonUI.Utils
+{
+    public static class Common
+    {
+        public static T FindLogicalParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentObject;
+
+            parentObject = LogicalTreeHelper.GetParent(child);
+
+            // get parent item
+            //DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            // we’ve reached the end of the tree
+            if (parentObject == null) return null;
+
+            // check if the parent matches the type we’re looking for
+            T parent = parentObject as T;
+            if (parent != null)
+            {
+                return parent;
+            }
+            else
+            {
+                // use recursion to proceed with next level
+                return FindLogicalParent<T>(parentObject);
+            }
+        }
+
+        public static T FindVisualParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            // get parent item
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            // we’ve reached the end of the tree
+            if (parentObject == null) return null;
+
+            // check if the parent matches the type we’re looking for
+            T parent = parentObject as T;
+            if (parent != null)
+            {
+                return parent;
+            }
+            else
+            {
+                // use recursion to proceed with next level
+                return FindVisualParent<T>(parentObject);
+            }
+        }
+
+        /// <summary>
+        /// Find specific child (name and type) from parent
+        /// </summary>
+        public static T FindChild<T>(DependencyObject parent, string childName) where T : DependencyObject
+        {
+            // Confirm parent and childName are valid. 
+            if (parent == null) return null;
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                // If the child is not of the request child type child
+                T childType = child as T;
+                if (childType == null)
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child, childName);
+
+                    // If the child is found, break so we do not overwrite the found child. 
+                    if (foundChild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childName))
+                {
+                    var frameworkElement = child as FrameworkElement;
+                    // If the child's name is set for search
+                    if (frameworkElement != null && frameworkElement.Name == childName)
+                    {
+                        // if the child's name is of the request name
+                        foundChild = (T)child;
+                        break;
+                    }
+                }
+                else
+                {
+                    // child element found.
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+
+            return foundChild;
+        }
+
+        public static DependencyObject FindChild(this DependencyObject reference, string childName, Type childType)
+        {
+            DependencyObject foundChild = null;
+            if (reference != null)
+            {
+                int childrenCount = VisualTreeHelper.GetChildrenCount(reference);
+                for (int i = 0; i < childrenCount; i++)
+                {
+                    var child = VisualTreeHelper.GetChild(reference, i);
+                    // If the child is not of the request child type child
+                    if (child.GetType() != childType)
+                    {
+                        // recursively drill down the tree
+                        foundChild = FindChild(child, childName, childType);
+                    }
+                    else if (!string.IsNullOrEmpty(childName))
+                    {
+                        var frameworkElement = child as FrameworkElement;
+                        // If the child's name is set for search
+                        if (frameworkElement != null && frameworkElement.Name == childName)
+                        {
+                            // if the child's name is of the request name
+                            foundChild = child;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        // child element found.
+                        foundChild = child;
+                        break;
+                    }
+                }
+            }
+            return foundChild;
+        }
+
+        /// <summary>
+        /// Gets scroll viewer of the object
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public static DependencyObject GetScrollViewer(DependencyObject o)
+        {
+            if (o is ScrollViewer)
+            { return o; }
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(o); i++)
+            {
+                var child = VisualTreeHelper.GetChild(o, i);
+
+                var result = GetScrollViewer(child);
+                if (result == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    return result;
+                }
+            }
+
+            return null;
+        }
+
+        public static object GetObjectDataFromPoint(ItemsControl source, Point point)
+        {
+            UIElement element = source.InputHitTest(point) as UIElement;
+            if (element != null)
+            {
+                //get the object from the element
+                object data = DependencyProperty.UnsetValue;
+                while (data == DependencyProperty.UnsetValue)
+                {
+
+                    // try to get the object value for the corresponding element
+                    data = source.ItemContainerGenerator.ItemFromContainer(element);
+
+                    //get the parent and we will iterate again
+                    if (data == DependencyProperty.UnsetValue)
+                        element = VisualTreeHelper.GetParent(element) as UIElement;
+
+                    //if we reach the actual listbox then we must break to avoid an infinite loop
+                    if (element == source || element == null)
+                        return null;
+                }
+
+                //return the data that we fetched only if it is not Unset value, 
+                //which would mean that we did not find the data
+                if (data != DependencyProperty.UnsetValue)
+                    return data;
+            }
+
+            return null;
+        }
+
+        // this method adds element in sorted manner
+        public static void AddSorted<T>(this IList<T> list, T item, IComparer<T> comparer = null)
+        {
+            if (comparer == null)
+                comparer = Comparer<T>.Default;
+
+            int i = 0;
+            while (i < list.Count && comparer.Compare(list[i], item) < 0)
+                i++;
+
+            list.Insert(i, item);
+        }
+
+        // this method checks if givven string contains special characters
+        public static bool HasSpecialCharacters(string str)
+        {
+            string specialCharacters = @"%!@#$%^&*()?/>.<,:;'\|}]{[~`+=-" + "\"";
+            char[] specialCharactersArray = specialCharacters.ToCharArray();
+
+            int index = str.IndexOfAny(specialCharactersArray);
+            //index == -1 no special characters
+            if (index == -1)
+                return false;
+            else
+                return true;
+        }
+
+        public static bool IsInteger(double value)
+        {
+            double diff = Math.Abs(Math.Truncate(value) - value);
+            return (diff < 0.0000001) || (diff > 0.9999999);
+        }
+    }
+}
